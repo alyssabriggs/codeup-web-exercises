8465bc4bc7b07f07eebdefb332c4c35e674c32db
alyssalanae <briggs.l.alyssa@gmail.com>
2019-10-08 10:51:03 -0500

    Functions-practice file

---
+++ js/functions-practice.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function multiplication(a,b) {
+    return a * b;
+}
+
+console.log(multiplication(4, 5));
+
+var division = function(c, d) {
+    return c / d ;
+};
+
+console.log(division(3, 4));
+
+
+// HOW TO WRITE AN IIFE FUNCTION ****
+(function () {
+
+})();
