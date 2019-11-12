03ec9783e5bba5d9b08a30aa59d6a08dddf121c6
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-12 09:44:50 -0600

    Warmup 11/12

--- js/keys.js
+++ js/keys.js
@@ -1,3 +1,5 @@
 var mapboxToken = "pk.eyJ1IjoiYWx5c3NhYnJpZ2dzIiwiYSI6ImNrMjJza2d4NjF5MTQzbm11ZHRybG85cmkifQ.MEsi7PM4fBC7CvLzeDaMSw";
 
 var darkSky = "8c777586a65e004b9b74efdaaca6043a";
+
+var giphy = "9tnKywI293vN8hWLyrrCD48BLWDaWD1P";
