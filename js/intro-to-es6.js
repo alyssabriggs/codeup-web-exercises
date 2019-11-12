a73ffce925a8b2d082d62a8edc036ec72169cf95
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-12 10:54:05 -0600

    ES6 lecture

--- js/intro-to-es6.js
+++ js/intro-to-es6.js
@@ -8,7 +8,7 @@
 //     Using the ES6 exponentiation Operator, refactor the following:
 
 
-var x = Math.pow(4,5);
+let x = Math.pow(4,5);
 console.log(x);
 
 let random = Math.floor(Math.random() * (2 ** 3));
@@ -18,8 +18,8 @@ console.log(random);
 /*********************************************
  *              let and const
  ******************************************** */
-// random = Math.floor(Math.random() * x);
-// console.log(random);
+random = Math.floor(Math.random() * x);
+console.log(random);
 
 // TO DO: Refactor everything above to use es6 variable types.
 //     What happens when you change the 'random' variable to a const?
@@ -29,18 +29,18 @@ console.log(random);
 // TO DO TOGETHER: change the var keyword to const. What do you see in
 // the console? Now change it to let and try again.
 
-// var sayHello = "Hello World!";
-//
-// sayHello = "Hello Deimos!";
-//
-// console.log(sayHello);
+let sayHello = "Hello World!";
+
+sayHello = "Hello Deimos!";
+
+console.log(sayHello);
 
 
 // TO DO: change var i into let. What are your results?
 
 // var numbers = [1,2,3,4,5];
 //
-// for(let i = 0; i < numbers.length; i++){
+// for(var i = 0; i < numbers.length; i++){
 //     console.log(i);
 // }
 // console.log(i);
@@ -52,8 +52,14 @@ console.log(random);
     // the array.
 const myArray = ["it", "is", "really", "cold", "outside"];
 
+for (let element of myArray){
+    console.log(element);
+}
+
 //TO DO: using a for ... of loop, iterate over the array of instructors. If
 // the instructor is Sophie or David, console log the name.
+
+
 
 const instructors = [
     'Daniel',
@@ -64,7 +70,31 @@ const instructors = [
     'Fer',
 ];
 
+const [a,b] = instructors;
+const europaInstructors = [a,b];
 
+console.log(europaInstructors);
+
+
+for (let instructor of instructors){
+    if (instructor === 'Sophie' || instructor === 'David'){
+        console.log(instructor);
+    }
+}
+
+const numbers = [1,2,3,4,5,6,7,8];
+
+function evenNumbers(arr) {
+    let newArray = [];
+    for (let number of numbers){
+        if (number % 2 === 0){
+            newArray.push(number);
+        }
+    }
+    return newArray;
+}
+
+console.log(evenNumbers(numbers));
 
 /*********************************************
  *              Arrow Functions
@@ -73,17 +103,25 @@ const instructors = [
 // TO DO TOGETHER: refactor this into an arrow function. Be as concise as
 // possible.
 
-function add(a,b){
-    return a + b;
-}
+// function add(a,b){
+//     return a + b;
+// }
+//
+const add = (a,b) => a + b;
+//
+// function square(x) {
+//     return x * x;
+// }
+
+const square = x => x * x;
 
 // TO DO TOGETHER: refactor the following so that variable 'greeting' has a
 // default value of 'Good Morning' and 'name' has the value of 'Deimos.
 //
 //      Check your work.
 
-const myGreeting = (greeting,name) => {
-    return `${greeting},${name}`;
+const myGreeting = (greeting = "Good Morning",name = "Deimos") => {
+    // return `${greeting},${name}`;
 };
 
 console.log(myGreeting());
@@ -94,16 +132,38 @@ console.log(myGreeting("Hello", "Sophie"));
  ******************************************** */
 
 // TO DO: Refactor the Object below to use shorthand property assignment
+//
+// const mydog = "Spike";
+// const cat = "Tom";
+// const mouse = "Jerry";
 
-const mydog = "Spike";
-const cat = "Tom";
-const mouse = "Jerry";
+// const pals = {
+//     dog: mydog,
+//     cat,
+//     mouse
+// };
 
 const pals = {
-    dog: mydog,
-    cat: cat,
-    mouse: mouse
+    dog: "Spike",
+    cat: "Tom",
+    mouse: "Jerry"
 };
+
+// let myDog = pals.dog;
+// let myCat = pals.cat;
+// let myMouse = pals.mouse;
+
+
+//Take properties dog and cat of the object pals
+//create variables out of them
+
+const {dog, cat, mouse} = pals;
+
+console.log(dog);
+console.log(cat);
+console.log(mouse);
+
+// console.log(pals);
 
 // TO DO TOGETHER: Use Object Destructuring to refactor
 
@@ -117,11 +177,22 @@ const myCar = {
     color
 };
 
-const carInfo = (car) => {
-    var make = car.make;
-    var model = car.model;
+const yourCar = {
+    make: 'Hyundai',
+    model: 'Sonata',
+    color: "pink"
+};
+
+// console.log(myCar.make);
+
+const carInfo = ({make,model}) => {
+    // let make = car.make;
+    // let model = car.model;
+    console.log("Here is your car info");
+    console.log(make, model)
 };
 
 carInfo(myCar);
+carInfo(yourCar);
 
 //BONUS: Array Destructuring.
