07bf2d8409e36e56407ef7b73c9f318629441484
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-13 16:31:08 -0600

    finished exercise

--- js/promises.js
+++ js/promises.js
@@ -1,23 +1,35 @@
 "use strict";
 
-function wait(num) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
+// function wait(num) {
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(function() {
+//             resolve();
+//         }, num);
+//     });
+// }
+
+const wait = num => {
+    return new Promise((resolve,reject) => {
+        setTimeout( () => {
             resolve();
-        }, num);
+            }, num)
     });
-}
+};
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
-function lastCommit(username) {
-    return new Promise(function (resolve, reject) {
-        resolve(fetch("https://api.github.com/users/" + username + "/events", {headers: {'Authorization': `token ${github}`}}));
-    })
+const lastCommit = (username) => {
+    fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${github}`}})
         .then(response => response.json())
-        .then(users => users.map(user => user.created_at).find(user => "PushEvent"));
-}
+        .then(data => data.find(event => event.type === "PushEvent"))
+            .then(event => event.created_at)
+            .then(date => console.log(date)
+        );
+
+        // .then(users => users.map(user => user.created_at).find(user => "PushEvent"));
+        // .then(users => users.map(user => user));
+};
 
 console.log(lastCommit("alyssabriggs"));
