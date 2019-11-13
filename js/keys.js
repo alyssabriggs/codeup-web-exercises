76c3ccfbe6b8fbf187784a42c403c5d476806e24
alyssalanae <briggs.l.alyssa@gmail.com>
2019-11-13 12:08:54 -0600

    Deleted keys

--- js/keys.js
+++
@@ -1,7 +0,0 @@
-var mapboxToken = "pk.eyJ1IjoiYWx5c3NhYnJpZ2dzIiwiYSI6ImNrMjJza2d4NjF5MTQzbm11ZHRybG85cmkifQ.MEsi7PM4fBC7CvLzeDaMSw";
-
-var darkSky = "8c777586a65e004b9b74efdaaca6043a";
-
-// var giphy = "9tnKywI293vN8hWLyrrCD48BLWDaWD1P";
-
-var github = "32ed6e3d994e9c9d3aba67c749d911490c60a485";
